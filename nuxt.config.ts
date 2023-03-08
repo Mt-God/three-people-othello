9d8dc8965a7684f0bed60a4eac043243f7d8afc4
Atsuya Yamagami <my2502@icloud.com>
2023-03-08 19:31:44 +0900

    add Vuetify settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,25 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['vuetify/styles'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins!.push(vuetify());
+      });
+    },
+  ],
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+    vue: {
+      template: { transformAssetUrls },
+    },
+  },
   typescript: {
     shim: false,
   },
