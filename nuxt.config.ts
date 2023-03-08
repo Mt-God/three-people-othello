1ef9b7538ff191cb7f7176972bb52da6b591bdd7
Atsuya Yamagami <my2502@icloud.com>
2023-03-08 18:16:46 +0900

    add typescript setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  typescript: {
+    shim: false,
+  },
 });
