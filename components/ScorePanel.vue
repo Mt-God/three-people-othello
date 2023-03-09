0508ce1e482f269b827286e0fa13cba3db52b17e
Atsuya Yamagami <my2502@icloud.com>
2023-03-09 18:12:35 +0900

    make ScorePanel component

---
+++ components/ScorePanel.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { useDisplay } from 'vuetify';
+
+const props = defineProps<{
+  names: {
+    player1: string;
+    player2: string;
+    player3: string;
+  };
+  scores: {
+    player1: number;
+    player2: number;
+    player3: number;
+  };
+}>();
+
+const { mdAndUp } = useDisplay();
+</script>
+
+<template>
+  <v-card
+    class="mx-auto"
+    width="fit-content"
+  >
+    <v-card-text :class="mdAndUp && 'd-flex'">
+      <ScorePanelText
+        :name="props.names.player1"
+        :score="props.scores.player1"
+        :color="'#f00'"
+      />
+      <v-divider
+        :vertical="mdAndUp"
+        class="border-opacity-25"
+      ></v-divider>
+      <ScorePanelText
+        :name="props.names.player2"
+        :score="props.scores.player2"
+        :color="'#080'"
+      />
+      <v-divider
+        :vertical="mdAndUp"
+        class="border-opacity-25"
+      ></v-divider>
+      <ScorePanelText
+        :name="props.names.player3"
+        :score="props.scores.player3"
+        :color="'#00f'"
+      />
+    </v-card-text>
+  </v-card>
+</template>
---
+++ components/ScorePanelText.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { mdiCircle } from '@mdi/js';
+
+const props = defineProps<{
+  name: string;
+  score: number;
+  color: string;
+}>();
+</script>
+
+<template>
+  <div class="text-sm-body-1 mx-2 my-1">
+    <v-icon
+      :icon="mdiCircle"
+      :color="props.color"
+    ></v-icon>
+    {{ props.name }}
+    <span class="float-right">{{ props.score }}</span>
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: 250px;
+}
+</style>
